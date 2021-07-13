32eb73b47905cd8366e531531bbe356e5998676e
Matti Cosentino <mattocosen@hotmail.com>
2021-07-13 19:22:26 -0300

    Creando repo de reactjs

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <p>Hola Mundo!!!!</p>;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
